ddea09c3f30144fe4b1ba13522c86441aeb3e318
shayandaneshvar <daneshvarshayan@gmail.com>
2020-06-18 11:30:51 +0430

    Add packages

---
+++ src/main/java/ir/ac/kntu/cs2d/client/ClientMain.java
@@ -0,0 +1,7 @@
+package ir.ac.kntu.cs2d.client;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        // TODO
+    }
+}
---
+++ src/main/java/ir/ac/kntu/cs2d/server/ServerMain.java
@@ -0,0 +1,5 @@
+package ir.ac.kntu.cs2d.server;
+
+public class ServerMain {
+    // TODO
+}
